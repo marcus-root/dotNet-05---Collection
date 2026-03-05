f4e41702cad5e318f5e4aadea064714a6c1eb212
Marcus Rehm <github.com-public@0n0.de>
2026-03-05 15:44:24 +0100

    1

---
+++ Collections - 02 - Dictionary - Worthäufigkeit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Collections___02___Dictionary___Worthäufigkeit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Collections - 02 - Dictionary - Worthäufigkeit/Wortzähler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Collections___02___Dictionary___Worthäufigkeit
+{
+    internal static class Wortzähler<TKey, TValue> where TKey : IComparable where TValue : IComparable
+    {
+        static Dictionary<TKey, TValue> dict = new Dictionary<TKey, TValue>();
+
+        static public int Count(String text)
+        {
+            String text2=Regex.Replace(text, @"\W", " ");
+            text2 = Regex.Replace(text2, @"[\s]+", "");
+
+        }
+    }
+}
